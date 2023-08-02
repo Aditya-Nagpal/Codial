a49ada0c3cde6d36021b34644d5a824ad7f5a4e7
Aditya-Nagpal <adityanagpal10@gmail.com>
2023-08-02 20:12:01 +0530

    commited

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port=9000;
 // use express router.
 app.use('/',require('./routes/index'));
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function (err){
     if(err){
         console.log(`Error in connecting to server ${err}`);
