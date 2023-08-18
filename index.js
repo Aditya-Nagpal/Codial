a4f9f7675a1e4b55e049a5f0c32e4100db03a16c
Aditya-Nagpal <adityanagpal10@gmail.com>
2023-08-18 21:15:05 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express=require('express');
 const app=express();
+const expressLayouts=require('express-ejs-layouts');
 const port=9000;
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 // use express router.
 app.use('/',require('./routes/index'));
