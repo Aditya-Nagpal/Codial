0b434b8b44cd22adeff2e4922a72db5dc791cd03
Aditya-Nagpal <adityanagpal10@gmail.com>
2023-08-25 19:36:00 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const expressLayouts=require('express-ejs-layouts');
+const db=require('./config/mongoose');
 const port=9000;
 
 app.use(express.static('./assets'));
