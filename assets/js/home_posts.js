055f494b2a5b87842953e979a08927c5bd5ef92a
Aditya-Nagpal <adityanagpal10@gmail.com>
2023-09-27 00:17:13 +0530

    Commit

---
+++ assets/js/home_posts.js
@@ -0,0 +1,23 @@
+{
+    // Method to submit the form data for new post using AJAX.
+    let createPost=function(){
+        let newPostForm=$('#new-post-form');
+        newPostForm.submit(function (e){
+            e.preventDefault();
+            $.ajax({
+                type: 'post',
+                url: '/posts/create',
+                data: newPostForm.serialize(),
+                success: function(data){
+                    console.log(data);
+                }, error: function(error){
+                    console.log(error.responseText);
+                }
+            });
+        });
+    };
+
+    // Method to create a post in DOM.
+
+    createPost();
+}
