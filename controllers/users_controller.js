710d34f51c7a973fc23a80b26ecc6eee27664282
Aditya-Nagpal <adityanagpal10@gmail.com>
2023-09-29 05:02:07 +0530

    Commit

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User=require('../models/user');
+const fs=require('fs');
+const path=require('path');
 
 module.exports.profile=async function (req,res){
     let user=await User.findById(req.params.id);
@@ -19,6 +21,11 @@ module.exports.update=async function (req,res){
                 user.name=req.body.name;
                 user.email=req.body.email;
                 if(req.file){
+                    if(user.avatar){
+                        if(fs.statSync(__dirname, '..',user.avatar).isFile()){
+                            fs.unlinkSync(path.join(__dirname, '..',user.avatar));
+                        }
+                    }
                     // this is saving the path of the uploaded file into the avatar field in the user
                     user.avatar=User.avatarPath + '/' + req.file.filename
                 }
