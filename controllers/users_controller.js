de1af515cc89f0f540b304fba8e396a8f97a71c4
Aditya-Nagpal <adityanagpal10@gmail.com>
2023-09-29 02:48:11 +0530

    Commit

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -9,15 +9,28 @@ module.exports.profile=async function (req,res){
 };
 
 module.exports.update=async function (req,res){
-    try {
-        if(req.user.id == req.params.id){
-            await User.findByIdAndUpdate(req.params.id, req.body);
-            return res.redirect('/');
-        } else{
-            return res.status(401).send('Unauthorized');
+    if(req.user.id == req.params.id){
+        try {
+            let user=await User.findById(req.params.id);
+            User.uploadedAvatar(req,res,function(err){
+                if(err){
+                    console.log("******Multer Error: ",err);
+                }
+                user.name=req.body.name;
+                user.email=req.body.email;
+                if(req.file){
+                    // this is saving the path of the uploaded file into the avatar field in the user
+                    user.avatar=User.avatarPath + '/' + req.file.filename
+                }
+                user.save();
+                return res.redirect('back');
+            })
+        } catch (error) {
+            
         }
-    } catch (error) {
-        console.log(error);
+    } else{
+        req.flash('error','Unauthorized!');
+        return res.status(401).send('Unauthorized');
     }
 };
 
