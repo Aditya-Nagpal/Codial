e9a84e4cdac77daf6c2c2b13d3b95756c0d27aff
Aditya-Nagpal <adityanagpal10@gmail.com>
2023-10-16 22:55:49 +0530

    Commit

---
+++ config/nodemailer.js
@@ -0,0 +1,35 @@
+const nodemailer=require('nodemailer');
+const ejs=require('ejs');
+const path=require('path');
+
+let transporter=nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'adityanagpal10@gmail.com',
+        pass: 'bhaibhai10'
+    }
+});
+
+let renderTemplate=(data,relativePath) => {
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath),
+        data,
+        function(err, template){
+            if(err){
+                console.log('Error in  rendering template',err);
+                return;
+            }
+            mailHTML=template;
+        }
+    );
+    return mailHTML;
+};
+
+module.exports={
+    transporter: transporter,
+    renderTemplate: renderTemplate
+};
