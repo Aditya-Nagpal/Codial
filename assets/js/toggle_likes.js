b1f033c3bae1d068096de81d7ee0590cce9f53c5
Aditya-Nagpal <adityanagpal10@gmail.com>
2024-01-11 02:40:51 +0530

    Commit

--- assets/js/toggle_likes.js
+++ assets/js/toggle_likes.js
@@ -30,7 +30,7 @@ class ToggleLike{
                 }
             })
             .fail(function(errData){
-                console.log('error in completing the request');
+                console.log('error in completing the request', errData);
             });
         });
     }
