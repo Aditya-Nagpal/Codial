91150a3dc3fac7fb9f86054b2e2e6dce15a4e597
Aditya-Nagpal <adityanagpal10@gmail.com>
2024-01-12 13:57:08 +0530

    Commit

---
+++ secrets.js
@@ -0,0 +1,12 @@
+module.exports={
+    clientID: '1074133619127-nagpj34f3okck28iuifccbs1fd3n939r.apps.googleusercontent.com',
+    clientSecret: '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',
+    callbackURL: 'http://localhost:9000/users/auth/google/callback',
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: 'mycodial@gmail.com',
+        pass: 'awwk cbpf tgpx hacu'
+    }
+};
