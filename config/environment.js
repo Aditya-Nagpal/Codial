27b7181ebdbe9115a080da44cf25a093a6f6a239
Aditya-Nagpal <adityanagpal10@gmail.com>
2024-01-14 20:15:31 +0530

    Commit

--- config/environment.js
+++ config/environment.js
@@ -20,7 +20,24 @@ const development={
 };
 
 const production= {
-    name: 'production'
+    name: 'production',
+    asset_path: process.env.ASSET_PATH,
+    session_cookie_key: process.env.CODIAL_SESSION_COOKIE_KEY,
+    db: process.env.CODIAL_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.CODIAL_GMAIL_USERNAME,
+            pass: process.env.CODIAL_GMAIL_PASSWORD
+        }
+    },
+    google_client_id: process.env.CODIAL_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.CODIAL_GOOGLE_CLIENT_SECRET,
+    google_callbackURL: process.env.CODIAL_GOOGLE_CALLBACK_URL,
+    jwt_secret: process.env.CODIAL_JWT_SECRET
 };
 
-module.exports=development;
+module.exports=eval(process.env.CODIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODIAL_ENVIRONMENT);
